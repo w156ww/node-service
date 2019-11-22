0893760dcec6e6744221125f591c4e160e5d3192
maple <746133313@qq.com>
2019-11-22 15:17:21 +0800

    删除 service 中 02 03 04 文件

--- src/service/01-router/index.js
+++ src/service/01-router/index.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const router = express.Router()
 
-// const projectSettingRouter = require("./project-setting-router")
-// const projectTplRouter = require('./project-tpl-router')
 const example = require('./example');
 const user = require('./distribute');
 
@@ -15,8 +13,6 @@ router.get("/getInfo", function (req, res) {
     res.send("122")
 })*/
 
-// projectSettingRouter.reqDistribute(router)
-// projectTplRouter.reqDistribute(router)
 example.reqDistribute(router);
 user.reqDistribute(router);
 
